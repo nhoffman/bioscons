be13118eb2590dfae649febfc5ebd244b26b55db
Noah Hoffman <noah.hoffman@gmail.com>
2011-03-29 17:28:33 -0700

    working on adding docs/conf.py as a target of increment_version.py

--- increment_version.py
+++ increment_version.py
@@ -21,27 +21,36 @@ def shell(args):
     
 def main():
 
-    if '-h' in sys.argv[1]:
+    if sys.argv[1:] and '-h' in sys.argv[1]:
         print __doc__
         sys.exit()
     
     current_ver = shell(["git", "describe"])
     ver_info = [int(x) for x in current_ver.split('.')]
     ver_info[-1] += 1
-    new_ver = '.'.join(str(x) for x in ver_info)
+    new_version = '.'.join(str(x) for x in ver_info[:2])
+    new_release = '.'.join(str(x) for x in ver_info)
 
-    print 'incrementing version:',current_ver,'--->', new_ver
+    print 'incrementing version:',current_ver,'--->', new_release
 
-    # update version in bioscons/__init__.py
+    print 'updating version in bioscons/__init__.py'
     for line in fileinput.input('bioscons/__init__.py', inplace=True):
         if line.startswith('__version__'):
-            sys.stdout.write('__version__ = "%s"\n' % new_ver)
-        else:
-            sys.stdout.write(line)
+            line = '__version__ = "%s"\n' % new_release
+        sys.stdout.write(line)
+    fileinput.close()
+
+    print 'updating version and release in docs/conf.py'
+    for line in fileinput.input('bioscons/__init__.py', inplace=True):
+        if line.startswith('version ='):
+            line = "version ='%s'\n" % new_version
+        elif line.startswith('release ='):
+            line = "release ='%s'\n" % new_release
+        sys.stdout.write(line)
     fileinput.close()
     
     # increment git tag
-    shell(['git','tag','-a','-m','"version increment using increment_version.py"','"%s"'%new_ver])
+    # shell(['git','tag','-a','-m','"version increment using increment_version.py"','"%s"'%new_ver])
     
 
 if __name__ == '__main__':
