ef71578d248f1419d892ff052cfd0095cb3b9c58
Noah Hoffman <noah.hoffman@gmail.com>
2011-09-20 14:51:10 -0700

    add pplacer.py module

--- bioscons/pplacer.py
+++ bioscons/pplacer.py
@@ -1,34 +1,110 @@
-# pplacer and utilities
-def pplacer_emitter(target, source, env):
-    refpkg, merged = source
-    target = os.path.splitext(str(merged))[0] + '.json'
-    return target, source
+"""
+Builders
+--------
 
-def pplacer_generator(source, target, env, for_signature):
-    """
-    target = [.json file (derived from query align)]
-    source = [refpkg, ref align, query align]
+"""
 
-    Will add flags defined by env['pplacer_flags']
+# we expect this to fail unless imported from within SConstruct
+try:
+    from SCons.Script import *
+except ImportError:
+    pass
+
+from taxtastic.refpkg import Refpkg
+
+from fileutils import rename
+from infernal import cmalign_method, cmmerge_method
+
+def pplacer(env, refpkg, alignment, outdir = None, options = None, nproc = 2):
     
-    Default flags are as follows:
-     * -p - calculate posterior probability and Bayes marginal likelihood
+    cmd = ['pplacer', '-j %i' % int(nproc)]
+
+    if outdir:
+        cmd.extend(['--out-dir', env.subst(outdir)])
+
+    if options:
+        cmd.append(options)
+
+    cmd.extend(['-c', '$SOURCES'])
     
-    See http://matsen.fhcrc.org/pplacer/manual.html
+    return env.Command(
+        target = rename(alignment, '.jplace'),
+        source = Flatten([refpkg, alignment]),
+        action = ' '.join(cmd)
+        )
+    
+def align_and_place(env, refpkg, qseqs, outdir = None,
+                    options = None, nproc = 1):
+
+    """
+    Align sequences in ``qseqs``, merge with the reference alignment,
+    and run pplacer.
+
+     * env - Environment instance.
+     * refpkg - path to a reference package directory.
+     * qseqs - unaligned query sequenecs in fasta format.
+     * options - command line options to be provided to
+       pplacer. Note that substitution of variables defined withon the
+       current environment will occur.
+     * outdir - optional output directory; saves files to same
+       directory as qseqs if unspecified.
+     * number of processors to use for ``cmalign`` and ``pplacer``.
+
+    Returns (sto, scores, merged, placefile)
+
+    Example::
+
+        from bioscons.pplacer import align_and_place
+        env.AddMethod(align_and_place, "align_and_place")    
+        sto, scores, merged, placefile = env.AlignAndPlace(
+            refpkg = 'my.refpkg', qseqs = 'myseqs.fasta',
+            pplacer_options = '-p --inform-prior --map-identity'
+        )
     """
 
-    try:
-        flags = env['pplacer_flags']
-    except KeyError:
-        flags = '-p' # calculate posterior probabilities by default
+    cmalign_options = '--hbanded --sub --dna -1'
+    
+    if not hasattr(env, 'cmalign_method'):
+        env.AddMethod(cmalign_method, 'cmalign_method')
 
-    refpkg, merged = source
-    outdir, _ = split(str(merged))
+    if not hasattr(env, 'cmmerge_method'):
+        env.AddMethod(cmmerge_method, 'cmmerge_method')
 
-    # -c Specify the path to the reference package.
-        
-    action='pplacer %(flags)s --out-dir "%(outdir)s" -c "%(refpkg)s" "%(merged)s"' % locals()
-    return action
+    if not hasattr(env, 'pplacer_method'):
+        env.AddMethod(pplacer, 'pplacer_method')
 
-pplacer = Builder(generator=pplacer_generator, emitter=pplacer_emitter)
+    pkg = Refpkg(refpkg)        
+    profile = pkg.file_abspath('profile')
+    ref_sto = pkg.file_abspath('aln_sto')
+    
+    # align sequences
+    sto, scores = env.cmalign_method(
+        profile = profile,
+        fasta = qseqs,
+        nproc = nproc,
+        options = cmalign_options,
+        outdir = outdir
+        )
+    
+    # merge with reference set
+    merged = env.cmmerge_method(
+        profile, ref_sto, sto,
+        outname = rename(sto, '_merged.sto'),
+        options = cmalign_options,
+        outdir = outdir
+        )
+
+    # pplacer!    
+    placefile = env.pplacer_method(
+        refpkg = refpkg,
+        alignment = merged,
+        outdir = outdir,
+        options = options,
+        nproc = nproc
+        )
+
+    if outdir and not outdir == '.':
+        Clean(placefile, Dir(outdir))
+    
+    return Flatten([sto, scores, merged, placefile])
 
