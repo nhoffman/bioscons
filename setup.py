ddd7224ba98efb4fd8ed595ddc3730788a6892d6
Connor McCoy <cmccoy@fhcrc.org>
2014-04-18 07:56:12 -0700

    move to setuptools
    For wheel support

--- setup.py
+++ setup.py
@@ -2,8 +2,14 @@
 install package:        python setup.py install
 create unix package:    python setup.py sdist
 """
+# Use setuptools, falling back on provided
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    import distribute_setup
+    distribute_setup.use_setuptools()
+    from setuptools import setup, find_packages
 
-from distutils.core import setup
 import glob
 
 from bioscons.__init__ import __version__
@@ -15,8 +21,7 @@ params = {'author': 'Noah Hoffman',
           'author_email': 'noah.hoffman@gmail.com',
           'description': 'Functions extending the scons build tool for reproducible research in bioinformatics.',
           'name': 'bioscons',
-          'package_dir': {'bioscons': 'bioscons'},
-          'packages': ['bioscons'],
+          'packages': find_packages(include=['bioscons*']),
           'scripts': scripts,
           'url': 'http://github.com/nhoffman/bioscons',
           'version': __version__,
