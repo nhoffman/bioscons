912ddd3210f075baa5cca28bf2d9b0ec670015f7
Chris Rosenthal <crosenth@uw.edu>
2014-06-10 11:01:41 -0700

    adding download url

--- setup.py
+++ setup.py
@@ -21,9 +21,10 @@ params = {'author': 'Noah Hoffman',
           'author_email': 'noah.hoffman@gmail.com',
           'description': 'Functions extending the scons build tool for reproducible research in bioinformatics.',
           'name': 'bioscons',
-          'packages': find_packages(include=['bioscons*']),
+          'packages': find_packages(),
           'scripts': scripts,
           'url': 'http://github.com/nhoffman/bioscons',
+          'download_url': 'http://pypi.python.org/pypi/bioscons',
           'version': __version__,
           'requires':['python (>= 2.7)','scons (>= 2.0)']}
 
