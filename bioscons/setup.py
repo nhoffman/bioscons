a31630451bc8f757be704bcca323ff8c4dff4703
Noah Hoffman <nhoffman@positron.local>
2015-01-01 19:44:34 -0800

    add setup module

---
+++ bioscons/setup.py
@@ -0,0 +1,44 @@
+import os
+
+try:
+    import SCons
+    from SCons.Script import (Help, GetOption, Environment, Variables)
+except ImportError:
+    # we expect this to fail unless imported from within SConstruct
+    pass
+
+
+def envsetup(vars=None, environ=None, PATH=None, bash=True, **kwargs):
+    vars = vars or Variables()
+    environ = os.environ.copy() if environ is None else environ
+
+    if PATH:
+        environ['PATH'] = PATH
+
+    env = Environment(ENV=environ, variables=vars, **kwargs)
+
+    if bash:
+        use_bash(env)
+
+    helpsetup(vars, env)
+
+    return env
+
+
+def use_bash(env, shellopts='errexit:pipefail'):
+    # SHELLOPTS sets shell options to fail (including piped
+    # commands) with nonzero exit status; this requires bash.
+    env['SHELL'] = 'bash'
+    env['ENV']['SHELLOPTS'] = shellopts
+
+
+def helpsetup(vars, env):
+    """Makes `scons -h` print help text.
+
+    """
+
+    Help(vars.GenerateHelpText(env))
+    if GetOption('help') and SCons.Node.Alias.default_ans.keys():
+        print 'Build Aliases:'
+        for alias in sorted(SCons.Node.Alias.default_ans.keys()):
+            print ' ', alias
--- bioscons/utils.py
+++ bioscons/utils.py
@@ -2,8 +2,7 @@ from os.path import join
 import os.path
 
 try:
-    import SCons
-    from SCons.Script import (PathVariable, Help, GetOption)
+    from SCons.Script import PathVariable
 except ImportError:
     # we expect this to fail unless imported from within SConstruct
     pass
@@ -103,15 +102,3 @@ def getvars(config, secnames, indir=None, outdir=None,
                 (varname, '%(varname)s: defined in [%(params)s]' % locals(), val))
 
     return tuple(vars)
-
-
-def helpsetup(vars, env):
-    """Makes `scons -h` print help text.
-
-    """
-
-    Help(vars.GenerateHelpText(env))
-    if GetOption('help') and SCons.Node.Alias.default_ans.keys():
-        print 'Build Aliases:'
-        for alias in sorted(SCons.Node.Alias.default_ans.keys()):
-            print ' ', alias
